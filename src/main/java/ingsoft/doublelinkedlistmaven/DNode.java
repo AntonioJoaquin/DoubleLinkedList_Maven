373b093a07fc18b3b129afed4e0d041395b6906d
AntonioJoaquin <antoniojoaquinluque04@gmail.com>
2015-04-16 20:38:38 +0200

    Version cuarta

---
+++ src/main/java/ingsoft/doublelinkedlistmaven/DNode.java
@@ -0,0 +1,24 @@
+package ingsoft.doublelinkedlistmaven;
+
+public class DNode {
+
+  // Data or reference to data
+  int data;
+  // A reference to the previous node
+  DNode previous;
+  // A reference to the next node
+  DNode next;
+
+  public DNode(int data) {
+    this.data = data;
+    this.previous = null;
+    this.next = null;
+  }
+
+  public DNode(int data, DNode previous, DNode next) {
+    this.data = data;
+    this.previous = previous;
+    this.next = next;
+  }
+
+}
---
+++ src/main/java/ingsoft/doublelinkedlistmaven/DoubleLinkedList.java
@@ -0,0 +1,70 @@
+package ingsoft.doublelinkedlistmaven;
+
+import java.util.*;
+
+public class DoubleLinkedList {
+
+  //points to first node of list
+  DNode first;
+  //points to last node of list
+  DNode last; 
+
+  public void insertAfter(List<DNode> list, DNode node, DNode newNode) {
+    newNode.previous = node;
+    newNode.next = node.next;
+    if (node.next == null) {
+      last = newNode;
+      node.next = newNode;
+    } else {
+      node.next.previous = newNode;
+      node.next = newNode;
+    }
+    list.add(newNode);
+  }
+
+  public void insertBefore(List<DNode> list, DNode node, DNode newNode) {
+    newNode.previous = node.previous;
+    newNode.next = node;
+    if (node.previous == null) {
+      first = newNode;
+      node.previous = newNode;
+    } else {
+      node.previous.next = newNode;
+      node.previous = newNode;
+    }
+    list.add(newNode);
+  }
+
+  public void insertBeginning(List<DNode> list, DNode newNode) {
+    if (first == null) {
+      first = newNode;
+      last = newNode;
+      newNode.previous = null;
+      newNode.next = null;
+    } else {
+      insertBefore(list, last, newNode);
+    }
+  }
+
+  public void insertEnd(List<DNode> list, DNode newNode) {
+    if (last == null) {
+      insertBeginning(list, newNode);
+    } else {
+      insertAfter(list, last, newNode);
+    }
+  }
+
+  public void remove(DNode node) {
+    if (node.previous == null) {
+      first = node.next;
+    } else if (node.next == null) {
+      last = node.previous;
+    } else {
+      node.previous.next = node.next;
+      node.next.previous = node.previous;
+    }
+
+  }
+
+}
+
